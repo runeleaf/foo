f649450704f1f44f061cfd0ecdce5afcd90832b8
runeleaf <runeleaf@gmail.com>
2013-05-31 18:12:02 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foo::Application.config.secret_key_base = '188537a822b6d5c196d30e304534045129ba40a1a2ef9cc5b8904ec31ec0fb68ecad844c6a24822ee465dcf48fd7fed5fc75dc369e904f53967165084de67a19'
